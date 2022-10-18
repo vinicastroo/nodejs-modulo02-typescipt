ff3e0bd0d239cbfbf5d737486a66fbc41e3864c6
vinicastroo <viniciusc.d.c@hotmail.com>
2022-10-18 17:56:13 -0300

    feat: creation repo

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { ListCategoriesUseCases } from "./ListCategoriesUseCases";
+
+class ListCategoriesController {
+  constructor(private listCategoriesUseCases: ListCategoriesUseCases) { }
+
+  handle(request: Request, response: Response) {
+    const all = this.listCategoriesUseCases.execute();
+
+    return response.json(all)
+  }
+}
+
+export {
+  ListCategoriesController
+}
---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCases.ts
@@ -0,0 +1,14 @@
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+class ListCategoriesUseCases {
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCases }
